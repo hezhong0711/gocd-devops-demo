a603d299f9d7b769f0bf8a1dde279bb027e4730c
HeZhong <zhong.he@thoughtworks.com>
2021-03-30 18:42:37 +0800

    fix env variable name

--- script/copy-deploy-file.sh
+++ script/copy-deploy-file.sh
@@ -6,11 +6,11 @@ set timeout 30
 set source [lindex $argv 0]
 set target [lindex $argv 1]
 
-spawn scp $source $env(APPLEHUB_USERNAME)@$env(APPLEHUB_HOST):$target
+spawn scp $source $env(SERVER_USERNAME)@$env(SERVER_HOST):$target
 expect {
   "(yes/no"
   { send "yes\n";exp_continue; }
   "password:"
-  { send "$env(APPLEHUB_PASSWORD)\n"; }
+  { send "$env(SERVER_PASSWORD)\n"; }
 }
 expect eof
