9d7a736cf07dc8069e481da8578a62b7832e82b8
HeZhong <zhong.he@thoughtworks.com>
2021-03-30 18:36:26 +0800

    fix env variable name

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/expect
 
 set timeout 30
-spawn ssh -p 22 $env(APPLEHUB_USERNAME)@$env(APPLEHUB_HOST) "docker-compose down && docker-compose pull && docker-compose up"
+spawn ssh -p 22 $env(SERVER_USERNAME)@$env(SERVER_HOST) "docker-compose down && docker-compose pull && docker-compose up"
 expect {
     "(yes/no)?"
     {send "yes\n";exp_continue;}
     "password:"
-    {send "$env(APPLEHUB_PASSWORD)\n";}
+    {send "$env(SERVER_PASSWORD)\n";}
 }
 
 expect eof
