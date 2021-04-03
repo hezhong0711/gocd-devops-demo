74729214c46698d6af0cead09201e270c2d90e76
hezhong0711 <hezhong0711@gmail.com>
2021-04-03 21:57:43 +0800

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 
 set timeout 30
-spawn ssh -p 22 $env(SERVER_USERNAME)@$env(SERVER_HOST) "docker-compose down && docker-compose pull && docker-compose up"
+spawn ssh -p 22 $env(SERVER_USERNAME)@$env(SERVER_HOST) "docker-compose down && docker-compose pull && docker-compose up -d"
 expect {
     "(yes/no)?"
     {send "yes\n";exp_continue;}
